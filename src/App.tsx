88bc1f3276dd9414d19a32ffb135d585e5c09406
thiagocabralcorreia <thiagocabralcorreia@gmail.com>
2024-07-14 13:19:17 -0300

    ✨ feat: add dropdown-menu and theme-toggle components

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,20 @@ import "./global.css";
 import { Helmet, HelmetProvider } from "react-helmet-async";
 import { RouterProvider } from "react-router-dom";
 import { Toaster } from "sonner";
+import { ThemeProvider } from "./components/theme/theme-provider";
 
 import { router } from "./routes";
 
 export function App() {
   return (
     <HelmetProvider>
-      <Helmet titleTemplate="%s | Cowabunga Pizza Shop" />
-      <Toaster richColors />
-      <RouterProvider router={router} />
+      <ThemeProvider defaultTheme="light" storageKey="pizzashop-theme">
+        <Helmet titleTemplate="%s | Cowabunga Pizza Shops" />
+
+        <Toaster richColors />
+
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </HelmetProvider>
   );
 }
--- src/pages/_layouts/auth.tsx
+++ src/pages/_layouts/auth.tsx
@@ -7,11 +7,11 @@ export function AuthLayout() {
       <div className="flex h-full flex-col justify-between border-r border-foreground/5 bg-muted p-10 text-muted-foreground">
         <div className="flex items-center gap-3 text-lg text-foreground">
           <Pizza className="h-5 w-5" />
-          <span className="font-semibold">Cowabunga Pizza Shop</span>
+          <span className="font-semibold">Cowabunga Pizza Shops</span>
         </div>
 
         <footer className="text-sm">
-          Painel do parceiro &copy; Cowabunga Pizza Shop -{" "}
+          Painel do parceiro &copy; Cowabunga Pizza Shops -{" "}
           {new Date().getFullYear()}
         </footer>
       </div>
